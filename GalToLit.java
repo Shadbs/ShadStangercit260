4cef42b0f30bcae67e547afc762e455ae9ec8d85
Shadbs <sbstanger@gmail.com>
2014-04-24 16:04:02 -0600

    Create GalToLit.java

---
+++ GalToLit.java
@@ -0,0 +1,12 @@
+public class GalToLit {
+	
+	public static void main(String args[]) {
+		double gallons; // number of gallons
+		double liters; // number of litres
+
+		gallons = 10;
+		liters = gallons *3.7854; // convert
+
+	System.out.println(gallons + " gallons is " + liters + " liters.");
+	}
+}
